3b6fda56accdc4b75acb44531cfc40c123520633
AdhikarShinde15 <adhikar.shinde@coditas.com>
2021-09-30 08:18:48 +0530

    Added Clip Path

--- src/components/DisplayQuote.js
+++ src/components/DisplayQuote.js
@@ -1,10 +1,12 @@
 import React from "react";
 
 const DisplayQuote = (props) => (
-    <div className="box">
+    <div className="card">
+      <div className="box">
         <p className="tag">{props.tag}</p>
-        <div className="content"><p><i className="fas fa-quote-left"></i>  {props.content}</p></div>
+         <div className="content"><p><i className="fas fa-quote-left"></i>  {props.content}</p></div>
         <p className="name">-{props.author}</p>
+    </div>
     </div>
 );
 
